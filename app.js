3db261e23f732bc2b763dc8f998908aff32dff1e
José Matheus <josematheusvenanciogomes@gmail.com>
2024-06-03 21:02:31 -0300

    Axios pt.1
    é otimizei o uso da api e criei a coleção de Logins no mongoDb, tentei ajustar o axios na tela de cadastro

--- app.js
+++ app.js
@@ -1,28 +1,96 @@
-const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes);
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://venanciodarko:mY1rhdcURunjTIz1@blueproject.gt0mef9.mongodb.net/";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+//app.js
+const {MongoClient, ObjectId} = require("mongodb");
+async function connect(){
+  if(global.db) return global.db;
+    const conn = await MongoClient.connect("mongodb+srv://venanciodarko:g$$w7E%40R2*npTBu@blueproject.nzhazel.mongodb.net/");
+  if(!conn) return new Error("Can't connect");
+    global.db = await conn.db("Logins");
+  return global.db;
 }
-run().catch(console.dir);
+
+const express = require('express');
+const app = express();         
+const port = 3000; //porta padrão
+const mongoose = require('mongoose');
+
+
+app.use(require('cors')());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//definindo as rotas
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+
+// GET dog
+router.get('/dog', async function(req, res, next) {
+  try{
+    const apidog = await fetch('https://dog.ceo/api/breed/hound/list');
+    res.json(await apidog.json());
+  }
+  catch(ex){
+    console.log(ex);
+    res.status(400).json({erro: `${ex}`});
+  }
+}) 
+
+/* GET aluno */
+router.get('/Logins/id', async function(req, res, next) {
+    try{
+      const db = await connect();
+      if(req.params.id)
+        res.json(await db.collection("Logins").findOne({_id: new ObjectId(req.params.id)}));
+      else
+        res.json(await db.collection("Logins").find().toArray());
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// POST /aluno
+router.post('/Logins', async function(req, res, next){
+    try{
+      const Logins = req.body;
+      const db = await connect();
+      res.json(await db.collection("Logins").insertOne(Logins));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// PUT /aluno/{id}
+router.put('/Logins/:id', async function(req, res, next){
+    try{
+      const Logins = req.body;
+      const db = await connect();
+      res.json(await db.collection("Logins").updateOne({_id: new ObjectId(req.params.id)}, {$set: Logins}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// DELETE /aluno/{id}
+router.delete('/Logins/:id', async function(req, res, next){
+    try{
+      const db = await connect();
+      res.json(await db.collection("Logins").deleteOne({_id: new ObjectId(req.params.id)}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
