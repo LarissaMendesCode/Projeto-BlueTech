235f706c5ad3ea613650c626529db5aaca3bffa6
Lucas Moreira <lucasmoreiraproenca@gmail.com>
2024-06-06 06:43:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,96 +1,100 @@
-//app.js
-const {MongoClient, ObjectId} = require("mongodb");
-async function connect(){
-  if(global.db) return global.db;
-    const conn = await MongoClient.connect("mongodb+srv://venanciodarko:g$$w7E%40R2*npTBu@blueproject.nzhazel.mongodb.net/");
-  if(!conn) return new Error("Can't connect");
-    global.db = await conn.db("Logins");
+const { MongoClient, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+//const fetch = require('node-fetch'); 
+
+const app = express();
+const port = 3000;
+
+/*
+lucasmoreiraproenca159
+5WH7kyN7YSRUTrsi
+*/
+async function connect() {
+  if (global.db) return global.db;
+  const conn = await MongoClient.connect("mongodb+srv://lucasmoreiraproenca159:5WH7kyN7YSRUTrsi@cluster0.9wubyl6.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true });
+  if (!conn) throw new Error("Can't connect");
+  global.db = conn.db("Logins");
   return global.db;
 }
 
-const express = require('express');
-const app = express();         
-const port = 3000; //porta padrão
-const mongoose = require('mongoose');
-
-
-app.use(require('cors')());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-//definindo as rotas
 const router = express.Router();
 
 router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 
-// GET dog
-router.get('/dog', async function(req, res, next) {
-  try{
+/*/ GET dog
+router.get('/dog', async (req, res) => {
+  try {
+    const { default: fetch } = await import('node-fetch');
     const apidog = await fetch('https://dog.ceo/api/breed/hound/list');
     res.json(await apidog.json());
-  }
-  catch(ex){
+  } catch (ex) {
     console.log(ex);
-    res.status(400).json({erro: `${ex}`});
+    res.status(400).json({ error: `${ex}` });
   }
-}) 
+});
+*/
+// GET Logins by ID or all
+router.get('/Logins/:id?', async (req, res) => {
+  try {
+    const db = await connect();
+    if (req.params.id) {
+      const result = await db.collection("Logins").findOne({ _id: new ObjectId(req.params.id) });
+      res.json(result);
+    } else {
+      const results = await db.collection("Logins").find().toArray();
+      res.json(results);
+    }
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ error: `${ex}` });
+  }
+});
 
-/* GET cadastro */
-router.get('/Logins/id', async function(req, res, next) {
-    try{
-      const db = await connect();
-      if(req.params.id)
-        res.json(await db.collection("Logins").findOne({_id: new ObjectId(req.params.id)}));
-      else
-        res.json(await db.collection("Logins").find().toArray());
-    }
-    catch(ex){
-      console.log(ex);
-      res.status(400).json({erro: `${ex}`});
-    }
-})
+// POST /Logins
+router.post('/Logins', async (req, res) => {
+  try {
+    const Logins = req.body;
+    const db = await connect();
+    const result = await db.collection("Logins").insertOne(Logins);
+    res.json(result);
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ error: `${ex}` });
+  }
+});
 
-// POST /cadastro
-router.post('/Logins', async function(req, res, next){
-    try{
-      const Logins = req.body;
-      const db = await connect();
-      res.json(await db.collection("Logins").insertOne(Logins));
-    }
-    catch(ex){
-      console.log(ex);
-      res.status(400).json({erro: `${ex}`});
-    }
-})
+// PUT /Logins/{id}
+router.put('/Logins/:id', async (req, res) => {
+  try {
+    const Logins = req.body;
+    const db = await connect();
+    const result = await db.collection("Logins").updateOne({ _id: new ObjectId(req.params.id) }, { $set: Logins });
+    res.json(result);
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ error: `${ex}` });
+  }
+});
 
-// PUT /cadastro/{id}
-router.put('/Logins/:id', async function(req, res, next){
-    try{
-      const Logins = req.body;
-      const db = await connect();
-      res.json(await db.collection("Logins").updateOne({_id: new ObjectId(req.params.id)}, {$set: Logins}));
-    }
-    catch(ex){
-      console.log(ex);
-      res.status(400).json({erro: `${ex}`});
-    }
-})
-
-// DELETE /cadastro/{id}
-router.delete('/Logins/:id', async function(req, res, next){
-    try{
-      const db = await connect();
-      res.json(await db.collection("Logins").deleteOne({_id: new ObjectId(req.params.id)}));
-    }
-    catch(ex){
-      console.log(ex);
-      res.status(400).json({erro: `${ex}`});
-    }
-})
+// DELETE /Logins/{id}
+router.delete('/Logins/:id', async (req, res) => {
+  try {
+    const db = await connect();
+    const result = await db.collection("Logins").deleteOne({ _id: new ObjectId(req.params.id) });
+    res.json(result);
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ error: `${ex}` });
+  }
+});
 
 app.use('/', router);
 
-//inicia o servidor
-app.listen(port);
-console.log('API funcionando!');
+app.listen(port, () => {
+  console.log(`API funcionando na porta ${port}!`);
+});
